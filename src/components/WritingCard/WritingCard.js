ac9b4ea10bc76c3c0e2c3dac2a2a87a3feb38ed5
Sam Green <sgreen@turinglab.co.uk>
2021-06-16 13:04:22 +0100

    javascript modification

--- src/components/WritingCard/WritingCard.js
+++ src/components/WritingCard/WritingCard.js
@@ -1,10 +1,13 @@
 import './WritingCard.css';
 
 function WritingCard(props) {
+
+  let title = props.title + " " + props.title
+
   return (
     <div className="wc-container">
       <img className="wc-image" src={props.image} alt={props.title}></img>
-      <h2 className="wc-title">{props.title}</h2>
+      <h2 className="wc-title">{title}</h2>
     </div>
   );
 }
